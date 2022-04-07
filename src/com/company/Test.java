642c6c2886204fdbcb822e5b75ff718627e229ec
sektor69 <sekt6969@gmail.com>
2022-04-07 13:16:55 +0300

    Hello

--- src/com/company/Test.java
+++ src/com/company/Test.java
@@ -3,6 +3,7 @@ package com.company;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
-
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
